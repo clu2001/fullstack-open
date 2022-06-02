792d3965cb0cd50b97700f1802aab714dae455d2
Calla Lu <46143943+clu2001@users.noreply.github.com>
2022-06-02 01:40:15 -0400

    exercise 1.1

---
+++ part1/introduction-to-react/src/App.js
@@ -0,0 +1,59 @@
+const Header = (props) => {
+
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      <p1>
+        {props.part1} {props.exercises1}
+      </p1>
+      <p2>
+        {props.part2} {props.exercises2}
+      </p2>
+      <p1>
+        {props.part3} {props.exercises3}
+      </p1>
+    </div>
+  )
+}
+
+const Total = (props) => {
+
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content part1={part1} exercises1={exercises1}
+              part2={part2} exercises2={exercises2}
+              part3={part3} exercises3={exercises3}
+      ></Content>
+      <Total exercises1={exercises1}
+            exercises2={exercises2}
+            exercises3={exercises3}
+      ></Total>
+
+    </div>
+  )
+}
+
+export default App
